e3ebb456814de8a5771c007ab7aad81eb6ebb55b
Yulia Barannikova <yulia.barannikova@gmail.com>
2015-06-24 16:57:16 -0400

    number of contracts methods

---
+++ trancheur.py
@@ -0,0 +1,30 @@
+class Bond():
+	def __init__(self, price, face_value, maturity, coupon):
+		self.price = price
+		self.face_value = face_value
+		self.maturity = maturity
+		self.coupon = coupon
+
+class Trancheur():
+	def __init__(self,bond,libor):
+		self.bond = bond
+		self.libor = libor
+		self.money_market_spread = 0.0005
+		self.floater_allocation = 0.80
+		self.residual_allocation = 0.20
+		self.floater_face = 1000
+		self.residual_face = 50000
+
+	def number_of_floater_contracts(self):
+		return int((self.bond.price - self.bond.face_value * (1-self.floater_allocation)) / self.floater_face)
+
+	def number_of_residual_contracts(self):
+		return int((self.bond.face_value * self.residual_allocation)/ self.residual_face)
+
+
+nyc = Bond(1000000, 1000000, 20, 0.05)
+t = Trancheur(nyc, 0.0015)	
+print ("---number of floater contracts----")
+print(t.number_of_floater_contracts())
+print ("---number of residual contracts----")
+print(t.number_of_residual_contracts())				

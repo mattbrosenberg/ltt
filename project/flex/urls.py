fe536051274cf5a1743e9e9305a4b6b35e7fc21a
mattbrosenberg <matthew.b.rosenberg@gmail.com>
2015-06-27 01:40:07 -0400

    created routes for navbar and built template for dashboard

--- project/flex/urls.py
+++ project/flex/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
-from .views import Index
+from .views import Index, Auctions, Portfolio, Account
 
 urlpatterns = patterns('',
 
-    url(r'^', Index.as_view()),
-    
+    url(r'^$', Index.as_view()),
+    url(r'^auctions/', Auctions.as_view()),
+    url(r'^portfolio/', Portfolio.as_view()),
+    url(r'^account/', Account.as_view()),
+
 )
--- project/flex/views.py
+++ project/flex/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.http import HttpResponse
 
 class Index(View):
 
     def get(self, request):
-        return HttpResponse("Welcome to the flex dashboard. <a href='/clients/logout/'>Log Out</a>")
+        return redirect("/flex/auctions/")
+
+
+class Auctions(View):
+
+    def get(self, request):
+        return render(request, "flex/auctions.html")
+
+
+class Portfolio(View):
+
+    def get(self, request):
+        return render(request, "flex/base.html")
+
+class Account(View):
+
+    def get(self, request):
+        return render(request, "flex/base.html")

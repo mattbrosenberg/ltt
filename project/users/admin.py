24cbb2facfe1cd75829d027ca8404c0d30042a4a
mattbrosenberg <matthew.b.rosenberg@gmail.com>
2015-07-01 18:24:24 -0400

    updated admin.py with new model name for Residual

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from users.models import User
-from trancheur.models import Bond, MoneyMarket, InverseFloater
+from trancheur.models import Bond, MoneyMarket, Residual
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Bond)
 admin.site.register(MoneyMarket)
-admin.site.register(InverseFloater)
+admin.site.register(Residual)
